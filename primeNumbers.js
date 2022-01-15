ed6965b9a6d6becce238b52cbf8efb1d298f184e
gökalp <avcu219@gmail.com>
2022-01-15 23:14:29 +0300

    add js

---
+++ primeNumbers.js
@@ -0,0 +1,12 @@
+// 1'e ve kendisine bölünecek, 1 den büyük pozitif tamsayı olacak
+
+function showPrimeNumbers(lowNumber, highNumber){
+    for (let i =lowNumber; i <= highNumber; i++ ){
+        let isPrime = true;
+        for (let j = 2; j <= i; j ++){
+            if( i % j ===0 && j !==i ){
+                isPrime = false
+            }
+        }
+    }
+}
